e9ae2288b6cf8dedad1ed0bea99c21bd53c9a5e1
Wojtek <wojtekf6@gmail.com>
2019-01-22 22:16:53 +0100

    upadte main

--- Projekt/main.cpp
+++ Projekt/main.cpp
@@ -1,7 +1,7 @@
-﻿#ifdef __cplusplus
-    #include <cstdlib>
+#ifdef __cplusplus
+#include <cstdlib>
 #else
-    #include <stdlib.h>
+#include <stdlib.h>
 
 #endif
 #ifdef __APPLE__
@@ -15,7 +15,6 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#include <string>
 using namespace std;
 
 
@@ -28,127 +27,69 @@ char const* tytul = "GKiM - Lab 2 - Nazwisko Imie";
 
 void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B);
 SDL_Color getPixel (int x, int y);
-
 void czyscEkran(Uint8 R, Uint8 G, Uint8 B);
 
+int img_width, img_height;
+SDL_Color ** pixels = nullptr;
 
-void Funkcja1();
-void Funkcja2();
-void Funkcja3();
-void Funkcja4();
-void Funkcja5();
-void Funkcja6();
-
-
-void Funkcja1() {
-
-int C,M,Y;
-int c,m,y,k;
-int R,G,B,W;
-int BW;
-SDL_Color kolor;
-
-struct rgb {
-    int r;
-    int g;
-    int b;
-
-};
-
-
-
-
-map<rgb, string, compEncode> encodeMap;
-
-
-
-for(int xx=0; xx<width/2; xx++)
+//funkcja tworzπca tablicÍ pixeli typu SDL_Color
+void PixelTable()
 {
-    for(int yy=0; yy<height/2; yy++)
-    {
-
-
-      SDL_Color kolor = getPixel(xx,yy); //we are gettig each pixel in img;
-
-     setPixel(xx+width/2,yy+height/2,kolor.r,kolor.g,kolor.b );
-
-
-     //setPixel(xx,yy+height/2,kolor.r,kolor.g,kolor.b);
-
-
-     //setPixel(xx+width/2,yy,kolor.r,kolor.g,kolor.b);
-
-
-
-
-
-    }
+    //tworzenie dynamicznej tablicy
+    pixels = new SDL_Color * [img_height];
+    
+    for (int i = 0; i < img_height; i++)
+        pixels[i] = new SDL_Color[img_width];
+    
+    //zape≥nianie tablicy
+    for (int i = 0; i < img_height; i++)
+        for (int j = 0; j < img_width; j++)
+            pixels[i][j] = getPixel(i, j);
 }
-
-for(int xx=0; xx<width/2; xx++)
-{
-    for(int yy=0; yy<height/2; yy++)
-    {
-
-    }
-}
-
-//...
-
-
-    SDL_Flip(screen);
-
-}
-
-
-
-
-
-
 
 void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B)
 {
-  if ((x>=0) && (x<width) && (y>=0) && (y<height))
-  {
-    /* Zamieniamy poszczególne składowe koloru na format koloru pixela */
-    Uint32 pixel = SDL_MapRGB(screen->format, R, G, B);
-
-    /* Pobieramy informacji ile bajtów zajmuje jeden pixel */
-    int bpp = screen->format->BytesPerPixel;
-
-    /* Obliczamy adres pixela */
-    Uint8 *p = (Uint8 *)screen->pixels + y * screen->pitch + x * bpp;
-
-    /* Ustawiamy wartość pixela, w zależności od formatu powierzchni*/
-    switch(bpp)
+    if ((x>=0) && (x<width) && (y>=0) && (y<height))
     {
-        case 1: //8-bit
-            *p = pixel;
-            break;
-
-        case 2: //16-bit
-            *(Uint16 *)p = pixel;
-            break;
-
-        case 3: //24-bit
-            if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-                p[0] = (pixel >> 16) & 0xff;
-                p[1] = (pixel >> 8) & 0xff;
-                p[2] = pixel & 0xff;
-            } else {
-                p[0] = pixel & 0xff;
-                p[1] = (pixel >> 8) & 0xff;
-                p[2] = (pixel >> 16) & 0xff;
-            }
-            break;
-
-        case 4: //32-bit
-            *(Uint32 *)p = pixel;
-            break;
-
+        /* Zamieniamy poszczególne składowe koloru na format koloru pixela */
+        Uint32 pixel = SDL_MapRGB(screen->format, R, G, B);
+        
+        /* Pobieramy informacji ile bajtów zajmuje jeden pixel */
+        int bpp = screen->format->BytesPerPixel;
+        
+        /* Obliczamy adres pixela */
+        Uint8 *p = (Uint8 *)screen->pixels + y * screen->pitch + x * bpp;
+        
+        /* Ustawiamy wartość pixela, w zależności od formatu powierzchni*/
+        switch(bpp)
+        {
+            case 1: //8-bit
+                *p = pixel;
+                break;
+                
+            case 2: //16-bit
+                *(Uint16 *)p = pixel;
+                break;
+                
+            case 3: //24-bit
+                if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+                    p[0] = (pixel >> 16) & 0xff;
+                    p[1] = (pixel >> 8) & 0xff;
+                    p[2] = pixel & 0xff;
+                } else {
+                    p[0] = pixel & 0xff;
+                    p[1] = (pixel >> 8) & 0xff;
+                    p[2] = (pixel >> 16) & 0xff;
+                }
+                break;
+                
+            case 4: //32-bit
+                *(Uint32 *)p = pixel;
+                break;
+                
+        }
+        /* update the screen (aka double buffering) */
     }
-         /* update the screen (aka double buffering) */
-  }
 }
 
 
@@ -168,7 +109,22 @@ void ladujBMP(char const* nazwa, int x, int y)
         SDL_Flip(screen);
         SDL_FreeSurface(bmp);
     }
-
+    
+    //niszczenie tablicy, chyba, øe nie istnieje
+    if (pixels != nullptr)
+    {
+        for (int i = 0; i < img_height; i++)
+            delete [] pixels[i];
+        delete pixels;
+    }
+    
+    //zapis rozdzielczoúci wyúwietlanego obrazka,
+    //øeby wiedzieÊ jak duøa zrobiÊ tablice pixeli
+    img_width = bmp->w;
+    img_height = bmp->h;
+    
+    //tworzenie tablicy
+    PixelTable();
 }
 
 
@@ -176,11 +132,7 @@ void czyscEkran(Uint8 R, Uint8 G, Uint8 B)
 {
     SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, R, G, B));
     SDL_Flip(screen);
-
 }
-
-
-
 
 SDL_Color getPixel (int x, int y) {
     SDL_Color color ;
@@ -201,36 +153,31 @@ SDL_Color getPixel (int x, int y) {
 }
 
 
-
-
-
-
-
 int main ( int argc, char** argv )
 {
     // console output
     freopen( "CON", "wt", stdout );
     freopen( "CON", "wt", stderr );
-
+    
     // initialize SDL video
     if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         printf( "Unable to init SDL: %s\n", SDL_GetError() );
         return 1;
     }
-
+    
     // make sure SDL cleans up before exit
     atexit(SDL_Quit);
-
+    
     // create a new window
     screen = SDL_SetVideoMode(width, height, 32,
-                                           SDL_HWSURFACE|SDL_DOUBLEBUF);
+                              SDL_HWSURFACE|SDL_DOUBLEBUF);
     if ( !screen )
     {
         printf("Unable to set video: %s\n", SDL_GetError());
         return 1;
     }
-
+    
     SDL_WM_SetCaption( tytul , NULL );
     // program main loop
     bool done = false;
@@ -243,20 +190,20 @@ int main ( int argc, char** argv )
             // check for messages
             switch (event.type)
             {
-                // exit if the window is closed
-            case SDL_QUIT:
-                done = true;
-                break;
-
-                // check for keypresses
-            case SDL_KEYDOWN:
+                    // exit if the window is closed
+                case SDL_QUIT:
+                    done = true;
+                    break;
+                    
+                    // check for keypresses
+                case SDL_KEYDOWN:
                 {
                     // exit if ESCAPE is pressed
                     if (event.key.keysym.sym == SDLK_ESCAPE)
                         done = true;
                     if (event.key.keysym.sym == SDLK_1)
                         Funkcja1();
-
+                    
                     if (event.key.keysym.sym == SDLK_a)
                         ladujBMP("obrazek1.bmp", 0, 0);
                     if (event.key.keysym.sym == SDLK_s)
@@ -269,14 +216,15 @@ int main ( int argc, char** argv )
                         ladujBMP("obrazek5.bmp", 0, 0);
                     if (event.key.keysym.sym == SDLK_b)
                         czyscEkran(0, 0, 10);          break;
-                     }
+                }
             } // end switch
         } // end of message processing
-
+        
     } // end main loop
-
-
+    
+    
     // all is well ;)
     printf("Exited cleanly\n");
     return 0;
 }
+

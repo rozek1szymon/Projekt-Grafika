b61d0adf807710b66ee91ceb4341b00f92a25ef4
Szymon Rozek <rozek1szymon@gmail.com>
2019-01-22 22:23:49 +0100

    null

--- Projekt/main.cpp
+++ Projekt/main.cpp
@@ -30,17 +30,23 @@ SDL_Color getPixel (int x, int y);
 void czyscEkran(Uint8 R, Uint8 G, Uint8 B);
 
 int img_width, img_height;
-SDL_Color ** pixels = nullptr;
+SDL_Color ** pixels = NULL;
 
+ Funkcja1()
+ {
+
+
+
+ }
 //funkcja tworzπca tablicÍ pixeli typu SDL_Color
 void PixelTable()
 {
     //tworzenie dynamicznej tablicy
     pixels = new SDL_Color * [img_height];
-    
+
     for (int i = 0; i < img_height; i++)
         pixels[i] = new SDL_Color[img_width];
-    
+
     //zape≥nianie tablicy
     for (int i = 0; i < img_height; i++)
         for (int j = 0; j < img_width; j++)
@@ -53,24 +59,24 @@ void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B)
     {
         /* Zamieniamy poszczególne składowe koloru na format koloru pixela */
         Uint32 pixel = SDL_MapRGB(screen->format, R, G, B);
-        
+
         /* Pobieramy informacji ile bajtów zajmuje jeden pixel */
         int bpp = screen->format->BytesPerPixel;
-        
+
         /* Obliczamy adres pixela */
         Uint8 *p = (Uint8 *)screen->pixels + y * screen->pitch + x * bpp;
-        
+
         /* Ustawiamy wartość pixela, w zależności od formatu powierzchni*/
         switch(bpp)
         {
             case 1: //8-bit
                 *p = pixel;
                 break;
-                
+
             case 2: //16-bit
                 *(Uint16 *)p = pixel;
                 break;
-                
+
             case 3: //24-bit
                 if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
                     p[0] = (pixel >> 16) & 0xff;
@@ -82,11 +88,11 @@ void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B)
                     p[2] = (pixel >> 16) & 0xff;
                 }
                 break;
-                
+
             case 4: //32-bit
                 *(Uint32 *)p = pixel;
                 break;
-                
+
         }
         /* update the screen (aka double buffering) */
     }
@@ -109,20 +115,20 @@ void ladujBMP(char const* nazwa, int x, int y)
         SDL_Flip(screen);
         SDL_FreeSurface(bmp);
     }
-    
+
     //niszczenie tablicy, chyba, øe nie istnieje
-    if (pixels != nullptr)
+    if (pixels !=NULL)
     {
         for (int i = 0; i < img_height; i++)
             delete [] pixels[i];
         delete pixels;
     }
-    
+
     //zapis rozdzielczoúci wyúwietlanego obrazka,
     //øeby wiedzieÊ jak duøa zrobiÊ tablice pixeli
     img_width = bmp->w;
     img_height = bmp->h;
-    
+
     //tworzenie tablicy
     PixelTable();
 }
@@ -158,17 +164,17 @@ int main ( int argc, char** argv )
     // console output
     freopen( "CON", "wt", stdout );
     freopen( "CON", "wt", stderr );
-    
+
     // initialize SDL video
     if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         printf( "Unable to init SDL: %s\n", SDL_GetError() );
         return 1;
     }
-    
+
     // make sure SDL cleans up before exit
     atexit(SDL_Quit);
-    
+
     // create a new window
     screen = SDL_SetVideoMode(width, height, 32,
                               SDL_HWSURFACE|SDL_DOUBLEBUF);
@@ -177,7 +183,7 @@ int main ( int argc, char** argv )
         printf("Unable to set video: %s\n", SDL_GetError());
         return 1;
     }
-    
+
     SDL_WM_SetCaption( tytul , NULL );
     // program main loop
     bool done = false;
@@ -194,7 +200,7 @@ int main ( int argc, char** argv )
                 case SDL_QUIT:
                     done = true;
                     break;
-                    
+
                     // check for keypresses
                 case SDL_KEYDOWN:
                 {
@@ -203,7 +209,7 @@ int main ( int argc, char** argv )
                         done = true;
                     if (event.key.keysym.sym == SDLK_1)
                         Funkcja1();
-                    
+
                     if (event.key.keysym.sym == SDLK_a)
                         ladujBMP("obrazek1.bmp", 0, 0);
                     if (event.key.keysym.sym == SDLK_s)
@@ -219,10 +225,10 @@ int main ( int argc, char** argv )
                 }
             } // end switch
         } // end of message processing
-        
+
     } // end main loop
-    
-    
+
+
     // all is well ;)
     printf("Exited cleanly\n");
     return 0;
